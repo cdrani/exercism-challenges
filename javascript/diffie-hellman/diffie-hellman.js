d89946036fd212ec35ce6ca31842d7e39aa6d26f
cdrainxv <cdrainxv@users.noreply.github.com>
2018-09-12 09:26:09 -0600

    remove comment cruft

--- javascript/diffie-hellman/diffie-hellman.js
+++ javascript/diffie-hellman/diffie-hellman.js
@@ -1,31 +1,22 @@
 export default class DiffieHellman {
   constructor(p, g) {
     this.goodArgs(p, g)
-
     this.p = p
     this.g = g
   }
 
   goodArgs(p, g) {
-    if(!(1 <= p && p < 9999) || !(1 <= g && g < 9999))
-            throw new Error("Arguments are out of range");
-   else if(!this.isPrime(p) || !this.isPrime(g))
-            throw new Error("Arguments are not prime");
+    if (!(1 <= p && p < 9999) || !(1 <= g && g < 9999))
+      throw new Error('Arguments are out of range')
+    else if (!this.isPrime(p) || !this.isPrime(g))
+      throw new Error('Arguments are not prime')
   }
-
 
   isPrime(num) {
-    for(let i = 2, s = Math.sqrt(num); i <= s; i++)
-      if(num % i === 0) return false; 
-      return num !== 1;
+    for (let i = 2, s = Math.sqrt(num); i <= s; i++)
+      if (num % i === 0) return false
+    return num !== 1
   }
-
-  // getPublicKeyFromPrivateKey(num) {
-  //   if (badKeys(num)) {
-  //     throw new Error()
-  //   }
-  //   return (this.g ** num) %  this.p
-  // }
 
   badKeys(num) {
     if (num <= 1 || num >= this.p) {
@@ -35,13 +26,13 @@ export default class DiffieHellman {
   }
 
   getPublicKeyFromPrivateKey(num) {
-   if(this.badKeys(num)) {
-     throw new Error()
-   } 
-     return  (this.g ** num) % this.p
+    if (this.badKeys(num)) {
+      throw new Error()
+    }
+    return this.g ** num % this.p
   }
 
   getSharedSecret(privKey, pubKey) {
-     return  (pubKey ** privKey) % this.p
+    return pubKey ** privKey % this.p
   }
-} 
+}
