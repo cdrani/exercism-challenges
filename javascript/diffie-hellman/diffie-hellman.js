94781c2e1428c6ac5c3d42330da0987d0c9b51db
cdrainxv <cdrainxv@users.noreply.github.com>
2018-09-12 09:22:17 -0600

    complete diffie-hellman exercise

--- javascript/diffie-hellman/diffie-hellman.js
+++ javascript/diffie-hellman/diffie-hellman.js
@@ -1,13 +1,16 @@
 export default class DiffieHellman {
   constructor(p, g) {
+    this.goodArgs(p, g)
+
     this.p = p
     this.g = g
-    this.a = ''
-    this.b = ''
-    this.A = ''
-    this.B = ''
-    this.sA = ''
-    this.sB = ''
+  }
+
+  goodArgs(p, g) {
+    if(!(1 <= p && p < 9999) || !(1 <= g && g < 9999))
+            throw new Error("Arguments are out of range");
+   else if(!this.isPrime(p) || !this.isPrime(g))
+            throw new Error("Arguments are not prime");
   }
 
 
@@ -17,27 +20,12 @@ export default class DiffieHellman {
       return num !== 1;
   }
 
-
-  goodArgs() {
-    if (this.p >= 0 && this.g >= 0) {
-      if(!this.isPrime(this.p) && !this.isPrime(this.g)) {
-        throw new Error()
-      }
-    }
-  }
-
-  getPublicKeyFromPrivateKey(num) {
-    if (badKeys(num)) {
-      throw new Error()
-    }
-    return (this.g ** num) %  this.p
-  }
-
-  randomNum(num) {
-    return Math.floor(Math.random() * (num - 1 + 1) + 1)
-  }
-
-  // this.goodArgs()
+  // getPublicKeyFromPrivateKey(num) {
+  //   if (badKeys(num)) {
+  //     throw new Error()
+  //   }
+  //   return (this.g ** num) %  this.p
+  // }
 
   badKeys(num) {
     if (num <= 1 || num >= this.p) {
@@ -46,18 +34,14 @@ export default class DiffieHellman {
     return false
   }
 
-  privateKeyGen(num) {
-   this.a = this.randomNum(this.p) 
-   this.b = this.randomNum(this.p) 
-   this.A = this.getPublicKeyFromPrivateKey(this.a)
-   this.B = this.getPublicKeyFromPrivateKey(this.b)
-    this.sA = this.secretKeyGen(this.B, this.a)
-    this.sB = this.secretKeyGen(this.A, this.b)
+  getPublicKeyFromPrivateKey(num) {
+   if(this.badKeys(num)) {
+     throw new Error()
+   } 
+     return  (this.g ** num) % this.p
   }
 
-  privateKeyGen(this.p)
-
-  getSharedSecret(num1, num2) {
-     return  (num1 ** num2) % this.p
+  getSharedSecret(privKey, pubKey) {
+     return  (pubKey ** privKey) % this.p
   }
 } 
